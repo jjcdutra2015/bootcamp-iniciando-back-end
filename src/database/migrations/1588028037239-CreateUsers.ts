c400a277d33dc10205a2203322f217e8dd3b6a0f
Julio Dutra <jjcdutra@gmail.com>
2020-04-27 21:06:35 -0300

    Relacionamento dos models

--- src/database/migrations/1588028037239-CreateUsers.ts
+++ src/database/migrations/1588028037239-CreateUsers.ts
@@ -9,7 +9,7 @@ export default class CreateUsers1588028037239 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
