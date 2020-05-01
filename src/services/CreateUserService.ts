c72c852fe76c144fe8fec590536f4c14457a3f92
Julio Dutra <jjcdutra@gmail.com>
2020-05-01 10:36:45 -0300

    Criando classe de erro

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,6 +1,8 @@
 import { getRepository } from 'typeorm';
 import { hash } from 'bcryptjs';
 
+import AppError from '../error/AppError';
+
 import User from '../models/User';
 
 interface Request {
@@ -18,7 +20,7 @@ class CreateUserService {
     });
 
     if (checkUserExists) {
-      throw new Error('E-mail Address already used.');
+      throw new AppError('E-mail Address already used.');
     }
 
     const hashPassword = await hash(password, 8);
