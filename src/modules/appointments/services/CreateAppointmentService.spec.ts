6afe8cc5d247117802bcd8691c7c6e8d8ae8a0ac
Julio Dutra <jjcdutra@gmail.com>
2020-06-15 21:54:44 -0300

    Criando o primeiro teste

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,22 @@
-test('sum two numbers', () => {
-  expect(1 + 2).toBe(3);
+import FakesAppointmentsRepository from '../repositories/fakes/FakesAppointmentsRepository';
+import CreateAppointmentService from './CreateAppointmentService';
+
+describe('CreateAppointment', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentsRepository = new FakesAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    const appointment = await createAppointment.execute({
+      provider_id: '123123',
+      date: new Date(),
+    });
+
+    expect(appointment).toHaveProperty('id');
+  });
+
+  // it('should not be able to create two appointments on the same time', () => {
+  //   expect(1 + 2).toBe(3);
+  // });
 });
