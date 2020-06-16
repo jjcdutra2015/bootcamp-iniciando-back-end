4bd86921506baa31a8908187801076495645f1ec
Julio Dutra <jjcdutra@gmail.com>
2020-06-16 20:25:15 -0300

    Teste de agendamento

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakesAppointmentsRepository from '../repositories/fakes/FakesAppointmentsRepository';
 import CreateAppointmentService from './CreateAppointmentService';
 
@@ -16,7 +17,22 @@ describe('CreateAppointment', () => {
     expect(appointment).toHaveProperty('id');
   });
 
-  // it('should not be able to create two appointments on the same time', () => {
-  //   expect(1 + 2).toBe(3);
-  // });
+  it('should not be able to create two appointments on the same time', async () => {
+    const fakeAppointmentsRepository = new FakesAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(
+      fakeAppointmentsRepository,
+    );
+
+    await createAppointment.execute({
+      provider_id: '123123',
+      date: new Date(),
+    });
+
+    expect(
+      createAppointment.execute({
+        provider_id: '123123',
+        date: new Date(),
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
