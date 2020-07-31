dd7dc0247dadbbb8c92c110b93100eb6795fb041
Julio Dutra <jjcdutra@gmail.com>
2020-07-30 21:00:15 -0300

    Configurando cache

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -11,12 +11,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-appointmentsRouter.post('/', celebrate({
-  [Segments.BODY]: {
-    provider_id: Joi.string().uuid().required(),
-    date: Joi.date().required(),
-  }
-}), appointmentsController.create);
-appointmentsRouter.post('/me', providerAppointmentsController.index);
+appointmentsRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      provider_id: Joi.string().uuid().required(),
+      date: Joi.date().required(),
+    },
+  }),
+  appointmentsController.create,
+);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
